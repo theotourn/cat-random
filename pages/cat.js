6954eb9cb64431f112026e1e5f60fad52cfa2d8b
Theo <theotournn@gmail.com>
2023-03-04 18:51:26 -0300

    button added below the cat image

--- pages/cat.js
+++ pages/cat.js
@@ -1,4 +1,5 @@
 import styles from "@/styles/Home.module.css";
+import Link from "next/link";
 
 const defaultEndpoint = "https://api.thecatapi.com/v1/images/search";
 
@@ -19,6 +20,11 @@ export default function Cat({ data }) {
       <div className={styles.imageContainer}>
         <img src={data[0].url} alt="Cat" className={styles.image} />
       </div>
+      <div className={styles.buttonContainer}>
+            <Link href="/cat">
+              <button className={styles.button}>Summon Another Cat</button>
+            </Link>
+          </div>
     </div>
   );
 }
